2f2005ce4ce6faec0341bfb52094af37b30ec56a
afiqmaznan <46741144+afiqmaznan@users.noreply.github.com>
2021-09-10 22:22:49 +0800

    Testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">Dashboard</div>
                 <div class="card-body">
-                    You are logged in.
+                    You are logged in!
                 </div>
             </div>
         </div>
